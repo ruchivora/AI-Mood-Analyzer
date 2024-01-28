51d150000ba4784e02ebc503a18a7414c22ff3e7
ruchivora <ruchi.v@beaconstac.com>
2024-01-28 23:18:57 +0530

    feature: Implement sentiment analysis

---
+++ app/(dashboard)/history/page.tsx
@@ -0,0 +1,37 @@
+import {getUserByClerkId} from "@/utils/auth";
+import {prisma} from "@/utils/db";
+import {all} from "deepmerge";
+import HistoryChart from "@/components/HistoryChart";
+
+const getData = async () => {
+  const user = await getUserByClerkId();
+  const analyses = await prisma.analysis.findMany({
+    where: {
+      userId: user.id
+    },
+    orderBy: {
+      createdAt: 'asc'
+    }
+
+  })
+
+  const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
+  const avg = Math.round(sum / analyses.length)
+
+  return {analyses, avg}
+}
+const History = async () => {
+  const {analyses, avg} = await getData();
+  console.log('analyses', analyses)
+
+  return (
+    <div className='h-full w-full'>
+      <div>{`Avg. Sentiment ${avg}`}</div>
+      <div className='p-4 h-full w-full'>
+        <HistoryChart data={analyses}/>
+      </div>
+    </div>
+  )
+}
+
+export default History
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 const links = [
   {href: '/', label: 'Home'},
   {href: '/journal', label: 'Journal'},
+  {href: '/history', label: 'History'},
 ]
 const DashboardLayout = ({children}) => {
     return <div className="h-screen w-screen relative">
